2f80a9a73978ddcb6f06b006ac98a5cb5e2e4eba
dario <hdcastanor@unal.edu.co>
2016-02-17 19:41:36 -0500

    Agregué la clase Escenario, y puse un fondo negro

---
+++ Escenario.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Escenario here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Escenario extends World
+{
+
+    /**
+     * Constructor for objects of class Escenario.
+     * 
+     */
+    public Escenario()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1);
+        setBackground("images/fondo.jpg");
+    }
+}
